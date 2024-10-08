a8f80c4aa5a1aaec4c84b47af0fa601722553697
Kin Hou Chan <126631989+KinHouChan@users.noreply.github.com>
2024-10-08 20:51:27 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,43 @@
+var vg_1 = "map_dv1.json";
+vegaEmbed("#map_death", vg_1).then(function(result) {
+// Access the Vega view instance
+// (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "death-cause-by-state.json";
+vegaEmbed("#death_cause", vg_2).then(function(result) {
+// Access the Vega view instance
+// (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 =  "health-exp.json";
+vegaEmbed("#health_exp", vg_3).then(function(result) {
+// Access the Vega view instance
+// (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "comet.json";
+vegaEmbed("#comet", vg_4).then(function(result) {
+// Access the Vega view instance
+// (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "bumpchart.json";
+vegaEmbed("#bumpchart", vg_5).then(function(result) {
+// Access the Vega view instance
+// (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_6 = "radarchart.json";
+vegaEmbed("#radarchart", vg_6).then(function(result) {
+// Access the Vega view instance
+// (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+
+var vg_8 = "donut-ethnic.json";
+vegaEmbed("#donut-ethnic", vg_8).then(function(result) {
+// Access the Vega view instance
+// (https://vega.github.io/vega/docs/api/view/) as result.view
+}
+).catch(console.error);
